41002005aaf73a0ee126ba63ea972cf9a0c3ab77
Jorge <Jorge@DESKTOP-5DL7PLT>
2023-08-20 23:18:01 -0600

    Agregar archivos de proyecto.

---
+++ Gasolina_3010746/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Gasolina_3010746
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double galones = Convert.ToDouble(canGas.Text);
+
+            double litros;
+
+            litros = galones * 3.78541;
+
+            cosTotal.Text = "El costo total es: $" + (litros * 1.168);
+        }
+    }
+}
---
+++ Gasolina_3010746/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Gasolina_3010746
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
